7f40b92fa65e4477abc5badb5b14e61f254e27f5
Ronald Casili <nvlled@gmail.com>
2018-01-12 21:31:11 +0800

    Fix indents and replace tabs with spaces

--- config.php
+++ config.php
@@ -3,11 +3,11 @@
 ##################################################
 #             Database configuration             #
 ##################################################
-# DB_HOST:	The MySQL server to connect to		 #
-# DB_USER:	The MySQL server username			 #
-# DB_PASS:	The MySQL server password			 #
-# DB_NAME:	The MySQL server database			 #
-# DB_TABLE:	The MySQL server table to create/use #
+# DB_HOST:  The MySQL server to connect to       #
+# DB_USER:  The MySQL server username            #
+# DB_PASS:  The MySQL server password            #
+# DB_NAME:  The MySQL server database            #
+# DB_TABLE: The MySQL server table to create/use #
 ##################################################
 
 define("DB_HOST", "localhost");
@@ -19,25 +19,25 @@ define("DB_TABLE", "images");
 ##################################################
 #              Folder configuration              #
 ##################################################
-# R_PATH:	The absolute path. Don't change this #
-#			unless you know what you're doing!	 #
-# F_PATH:	The folder to store images			 #
-#												 #
-#			INFO: This folder is relative to the #
-#			location of your form upload		 #
-#			handler. (eg: upload.php)			 #
-# H_FILE:	Change this to 'true' if you want    #
-#			to create/use a .htaccess file to    #
-#			protect your images folder.			 #
-#												 #
-#			WARNING: htaccess files are not 100% #
-#			reliable! It's STRONGLY advised to   #
-#			use a folder outside of your		 #
-#			document root instead! This option   #
-#			is only there for those who are		 #
-#			unable to do so and therefor have no #
-#			other choice but to rely on			 #
-#			htaccess!							 #
+# R_PATH: The absolute path. Don't change this   #
+#   unless you know what you're doing!           #
+# F_PATH: The folder to store images             #
+#                                                #
+#   INFO: This folder is relative to the         #
+#   location of your form upload                 #
+#   handler. (eg: upload.php)                    #
+# H_FILE: Change this to 'true' if you want      #
+#   to create/use a .htaccess file to            #
+#   protect your images folder.                  #
+#                                                #
+#   WARNING: htaccess files are not 100%         #
+#   reliable! It's STRONGLY advised to           #
+#   use a folder outside of your                 #
+#   document root instead! This option           #
+#   is only there for those who are              #
+#   unable to do so and therefor have no         #
+#   other choice but to rely on                  #
+#   htaccess!                                    #
 ##################################################
 
 define("R_PATH", __DIR__);
@@ -47,14 +47,14 @@ define("H_FILE", false);
 ##################################################
 #              File configuration                #
 ##################################################
-# F_SIZE:	The maximum file size in KB or MB	 #
-#			Example: 512K / 2M					 #
-#												 #
-#			WARNING: Make sure to check the		 #
-#			values of 'post_max_size' and		 #
-#			'upload_max_filesize' in your		 #
-#			php.ini file! This setting should	 #
-#			not be larger than either of those!	 #
+# F_SIZE: The maximum file size in KB or MB      #
+#   Example: 512K / 2M                           #
+#                                                #
+#   WARNING: Make sure to check the              #
+#   values of 'post_max_size' and                #
+#   'upload_max_filesize' in your                #
+#   php.ini file! This setting should            #
+#   not be larger than either of those!          #
 ##################################################
 
 define("F_SIZE", "4M");
