65c1b2a3fd026cd6ee8d4dd35b9fa554e1f64905
Bjorn van de Peut <bjorno43@hotmail.com>
2019-12-23 12:38:47 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
-<!DOCTYPE HTML>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Test</title>
-    </head>
-    <body>
-        <form name="upload" action="upload.php" method="POST" enctype="multipart/form-data">
-                Select image to upload: <input type="file" name="image[]" multiple>
-                <input type="submit" name="upload" value="upload">
-        </form>
-    </body>
-</html>
+	<!DOCTYPE HTML>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title>Test</title>
+		</head>
+		<body>
+			<img id="loadImg" src="load.gif" style="display:none;">
+			<form id="formUploadFile" name="upload" action="upload.php" method="POST" enctype="multipart/form-data">
+				Select image to upload: <input type="file" name="image[]" multiple>
+				<input type="submit" name="upload" value="upload">
+			</form>
+		</body>
+	</html>
--- upload.php
+++ upload.php
@@ -7,17 +7,17 @@ $img = new ImageUpload;
 $result = $img->uploadImages($_FILES['image']);
 
 if(!empty($result->info)){
-    foreach($result->info as $infoMsg){
-        echo $infoMsg .'<br />';
-    }
+	foreach($result->info as $infoMsg){
+		echo $infoMsg .'<br />';
+	}
 }
 
 echo "Your images can be viewed here:<br/><br/>";
 
 if(!empty($result->ids)){
-    foreach($result->ids as $id){
-        echo "https://your_website.com/image.php?". $id;
-    }
+	foreach($result->ids as $id){
+		echo "http://home.icecub.nl/image.php?". $id;
+	}
 }
 
 
